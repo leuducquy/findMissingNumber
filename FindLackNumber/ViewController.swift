5dad52aac975204fa9edd2e9e667977ffc6de828
Quy LD <quy-ld@trente.asia>
2016-08-29 13:53:38 +0700

    quy commit

--- FindLackNumber/ViewController.swift
+++ FindLackNumber/ViewController.swift
@@ -12,9 +12,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let arrayNumber = [9, 1, 2, 3, 4, 5, 7, 8, 0];
+        let arraySort = arrayNumber.sort { $0 < $1 }
+        
+        findMissingNumber(arraySort, first: 0)
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func findMissingNumber(array: [Int], first: Int) {
+        
+        for i in 1..<array.count {
+            for j in (1 + array[i - 1])..<array[i] {
+                print("so bi thieu : \(j)")
+                
+            }
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
